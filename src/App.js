be11c3d058f93bb3494a8aba2122a95a3d268ca1
Achieve-software <vtcbcdtyr@gmail.com>
2023-10-24 01:19:31 +0300

    DONE

--- src/App.js
+++ src/App.js
@@ -7,28 +7,30 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import PersonDetail from "./pages/PersonDetail";
 import Login from "./pages/Login";
 import { LoginContext } from "./context/LoginContext";
-import { useState } from "react"
+import { useState } from "react";
+import PrivateRouter from "./pages/PrivateRouter";
 function App() {
-
   //? Local State
-  const [user, setUser] = useState({ email: "", password: "" })
+  const [user, setUser] = useState({ email: "", password: "" });
 
   return (
     <LoginContext.Provider value={{ user, setUser }}>
-    <BrowserRouter>
-      <Navs />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="login" element={<Login />} />
+      <BrowserRouter>
+        <Navs />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="login" element={<Login />} />
 
-        <Route path="people" element={<People />} />
-        <Route path="people/:id" element={<PersonDetail />} />
+          <Route path="people" element={<PrivateRouter />}>
+            <Route path="" element={<People />} />
+            <Route path=":id" element={<PersonDetail />} />
+          </Route>
 
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </LoginContext.Provider>
   );
 }
